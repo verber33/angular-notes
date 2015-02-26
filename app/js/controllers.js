fda15d3d975f623b471c0bdbd319ed5aa94f84c3
andrey.verbytskyi <andrey.verbytskyi@symmetrics.de>
2015-02-27 00:08:16 +0200

    notes-dev: - Creating app.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,36 +8,86 @@ var phonecatControllers = angular.module('notesControllers', []);
 /**
  * Global Controller.
  */
-phonecatControllers.controller('globalCtrl', ['$scope', '$http', '$location',
-    function($scope, $http,  $location) {
-        $http.get('json/navigation.json').success(function(data) {
-            $scope.navigation = data;
-            console.log($scope.navigation);
-        });
+phonecatControllers.controller('globalCtrl', ['$scope',
+    function($scope) {
+        $scope.title = 'Welcome to your Notes list!';
 
-        $scope.activePage = function(urlPath) {
-            return urlPath == $location.path();
+        $scope.notes = [
+            {
+                title: 'First note',
+                description: 'Get up at 7:00 AM',
+                createdOn: 1424968457061,
+                deadline: '7:15 AM'
+            },
+            {
+                title: 'Second note',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                createdOn: 1424968482805,
+                deadline: 'Tomorrow'
+            },
+            {
+                title: 'Third note',
+                description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                createdOn: 1424968482805,
+                deadline: 'in December'
+            }
+        ];
+
+        $scope.getDate = function(date) {
+            var today = new Date(date),
+                day = today.getDate(),
+                month = today.getMonth() + 1,
+                year = today.getFullYear(),
+                hour = today.getHours(),
+                min = today.getMinutes();
+
+            if (day < 10) day = '0' + day;
+            if (month < 10) month = '0' + month;
+
+            return hour + ":" + min + " - " + day + '/' + month +'/' + year;
         };
 
-        $scope.goBack = function() {
-            history.back();
+
+        $scope.addNote = function(newTitle, newDesc, newDeadline) {
+            if (newTitle && newDesc && newDeadline) {
+                var note = {};
+
+                note['title'] = newTitle;
+                note['description'] = newDesc;
+                note['deadline'] = newDeadline;
+                note['createdOn'] = new Date().getTime();
+
+                if($scope.notes.indexOf(note) == -1) {
+                    $scope.notes.push(note);
+                }
+            }
         };
+
+        $scope.removeNote = function(time) {
+            $scope.notes.forEach(function(element, index, array){
+                if (element.createdOn == time) {
+                    array.splice(index, 1);
+                }
+            });
+        };
+
+
 
     }]);
 
 /**
  * Home page Controller.
  */
-phonecatControllers.controller('homeCtrl', ['$scope',
+/*phonecatControllers.controller('homeCtrl', ['$scope',
     function($scope) {
         $scope.title = 'Welcome to your Notes list!';
-    }]);
+    }]);*/
 
 /**
  * Catalog Controller.
  */
-phonecatControllers.controller('listCtrl', ['$scope',
+/*phonecatControllers.controller('listCtrl', ['$scope',
     function($scope) {
 
-    }]);
+    }]);*/
 
